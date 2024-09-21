aeedb7b4cf68c21e3ce20405c1975d42e3b347cb
Kunszt Norbert <norbi@manystack.com>
2024-09-21 17:34:43 +0200

    Modify ingredint design

--- src/components/Ingredients/Ingredients.tsx
+++ src/components/Ingredients/Ingredients.tsx
@@ -1,13 +1,13 @@
 const Ingredients = ({ ingredients }: any) => {
     if (!ingredients) ingredients = []
     return (
-        <fieldset className="text-xl max-w-sm border-4 border-green-500 rounded-lg p-2 m-auto">
-            <legend className="px-2 text-xl font-semibold underline decoration-green-500/60 decoration-2">Ingredients</legend>
+        <fieldset className="text-xl max-w-sm border-4 border-grey-500 rounded-lg p-2 m-auto">
+            <legend className="px-2 text-xl font-semibold underline  decoration-2">Ingredients</legend>
             <div className="flex flex-col gap-2 px-2 text-md font-serif">
                 {ingredients.map((ingredient: any, index: number) => {
                     return (
                         <>
-                            <a key={index}>{ingredient} {ingredients.length}{index}</a>
+                            <a key={index}>{ingredient} </a>
                             {(index===ingredients.length-1)?"":<hr />}
                         </>
                     )
